cf845353e0d739428fb614af688214e33e70ad39
Ewelina <ewelinazawol@gmail.com>
2022-07-20 19:48:41 +0100

    Add List in TileList

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,10 +1,17 @@
 import React from "react";
+import PropTypes from 'prop-types';
+import {Tile} from '../tile/Tile';
 
-export const TileList = () => {
+export const TileList = ({contacts}) => {
   return (
-    <div>
-      
-    </div>
+    <ul>
+      {contacts.map((contact, index) => {
+        <Tile key={index} contact={contact}/>
+      })}
+    </ul>
   );
 };
 
+TileList.propTypes = {
+  contacts: PropTypes.array
+}
